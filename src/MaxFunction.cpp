c0d43fbccdf0652f67ff4c1af998bd05df9d07d1
Dario <zurlo.dario@gmail.com>
2023-09-30 14:24:39 +0200

    modified fitness penalization in the example

--- src/MaxFunction.cpp
+++ src/MaxFunction.cpp
@@ -11,12 +11,12 @@ double MaxFunction::evaluateCostFunction(const std::vector<double> &values)
 {   
     double x = values.at(0);
     //double fitness = -std::pow(x,4) + 20*std::pow(x,3) - 150*std::pow(x,2) + 500*x;
-    double fitness = -std::pow(x,4) - 8*std::pow(x,3) + 18*x;
+    //double fitness = -std::pow(x,4) - 8*std::pow(x,3) + 18*x;
     //fitness = -fitness;
-    //double x_square = -std::pow(x-2, 2);
+    double fitness = -std::pow(x-2, 2);
     if(x > 10 || x < 0)
     {
-        fitness /= 1000;
+        fitness *= 0.5;
     }
     return fitness;
 
